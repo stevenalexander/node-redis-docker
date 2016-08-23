15487cea2121f791e5c27b9f205a18ad2515bf97
Steven Alexander <steven.william.alexander@googlemail.com>
2016-08-23 16:46:39 +0100

    cleaned skeleton and added standardjs

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # node-redis-docker
 
 Simple containerised Node JS Express application connected to Redis
+
+Notes
+
+* Generated skeleton express site via express generator `npm install express-generator -g`
+* Installed nodemon to reload on changes `npm install -g nodemon`
+* Run site with nodemon `DEBUG=app:* nodemon ./bin/www` or normally `DEBUG=app:* npm start`
+* Install node redis client with `npm install redis --save`
+* Installed Standard, globally `npm install standard -g` and locally `npm install standard --save-dev` for linting and added test script task
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+// var redis = require('redis')
+// var client = redis.createClient()
+
+// client.on('error', function (err) {
+//  console.log('Error ' + err)
+// })
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res) {
+  res.render('index', {title: 'Node Redis'})
+})
 
-module.exports = router;
+router.post('/redis', function (req, res) {
+  var test = req.body.test
+
+  console.log('test: ' + test)
+
+  // redis integration
+  res.redirect('/')
+})
+
+module.exports = router

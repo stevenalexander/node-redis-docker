7f005db3903c4ae10e57a57bec471e1de5855540
Steven Alexander <steven.william.alexander@googlemail.com>
2016-08-24 11:24:43 +0100

    added redis logic to node app

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Notes
 * Run site with nodemon `DEBUG=app:* nodemon ./bin/www` or normally `DEBUG=app:* npm start`
 * Install node redis client with `npm install redis --save`
 * Installed Standard, globally `npm install standard -g` and locally `npm install standard --save-dev` for linting and added test script task
+* Run redis container with `docker run --rm  --name some-redis -p 6379:6379 redis redis-server --appendonly yes`
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,35 @@
 var express = require('express')
 var router = express.Router()
 
-// var redis = require('redis')
-// var client = redis.createClient()
+var redis = require('redis')
+var client = redis.createClient()
 
-// client.on('error', function (err) {
-//  console.log('Error ' + err)
-// })
+client.on('error', function (err) {
+  console.log('Error ' + err)
+})
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', {title: 'Node Redis'})
+  var values = []
+
+  client.hgetall('values', function (err, obj) {
+    if (err) {
+      console.log('Error ' + err)
+      res.render('index', {title: 'Node Redis', values: null})
+    } else {
+      console.dir(obj)
+      values.push(obj.test)
+      res.render('index', {title: 'Node Redis', values: values})
+    }
+  })
 })
 
 router.post('/redis', function (req, res) {
   var test = req.body.test
 
   console.log('test: ' + test)
+
+  client.hmset('values', { 'test': test })
 
   // redis integration
   res.redirect('/')
